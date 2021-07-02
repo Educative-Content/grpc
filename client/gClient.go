9a59e6bd5c37ec50b66433479f0e77895a0d1f4b
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-07-02 12:52:12 +0300

    Initial versions gRPC CH12

--- client/gClient.go
+++ client/gClient.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/mactsouk/protobuf"
+	"google.golang.org/grpc"
+)
+
+var port = ":8080"
+
 func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("Using default port:", port)
+		return
+	} else {
+		port = os.Args[1]
+	}
+
+	conn, err := grpc.Dial(port, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("Dial:", err)
+		return
+	}
+
+	client := protobuf.NewMessageService(conn)
 
 }
--- server/gServer.go
+++ server/gServer.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+var port = ":8080"
+
 func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("Using default port:", port)
+		return
+	} else {
+		port = os.Args[1]
+	}
 
 }
